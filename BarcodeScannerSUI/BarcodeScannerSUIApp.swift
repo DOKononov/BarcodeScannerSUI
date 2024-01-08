a3083d6497d5a1710097745f17e270f9a2cfc9fb
d.o.kononov <d.o.kononov@gmail.com>
2024-01-08 09:12:33 +0300

    Initial Commit

---
+++ BarcodeScannerSUI/BarcodeScannerSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerSUIApp.swift
+//  BarcodeScannerSUI
+//
+//  Created by Dmitry Kononov on 8.01.24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
