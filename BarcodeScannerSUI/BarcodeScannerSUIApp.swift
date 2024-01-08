241cdab7025a380b54cafb6acd945e1a5ae36b3f
d.o.kononov <d.o.kononov@gmail.com>
2024-01-08 09:18:13 +0300

    init()

--- BarcodeScannerSUI/BarcodeScannerSUIApp.swift
+++ BarcodeScannerSUI/BarcodeScannerSUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BarcodeScannerSUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BarcodeScannerView()
         }
     }
 }
